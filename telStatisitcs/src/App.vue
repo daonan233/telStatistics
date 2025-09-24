ffc03785f3cf452498aebb611a775a5144f1cccb
daonan <2894566646@qq.com>
2025-09-24 17:40:16 +0800

    (feat):front-end and back-end

---
+++ telStatisitcs/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from 'vue-router'
+// 导入echarts
+import { provide } from 'vue'
+
+</script>
+
+<template>
+  <!-- 一级路由出口 -->
+  <router-view></router-view>
+</template>
+
+<style>
+html, body, #app{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
---
+++ telStatisitcs/src/router/index.js
@@ -0,0 +1,43 @@
+import {createRouter,createWebHistory} from 'vue-router'
+import Home from '@/pages/Home.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            redirect: '/home',
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home,
+        },
+
+    ]
+})
+
+// 创建全局路由守卫
+// 创建全局路由守卫
+router.beforeEach((to, from, next) => {
+    // 1、获取token
+    const token = localStorage.getItem('token')
+    // 2、判断是否需要登录权限
+    if (to.meta.requiresAuth) {
+        // 需要登录权限
+        if (token) {
+            next()
+        } else {
+            next({
+                path: '/login',
+                // 保存了用户原本要进入的url，当用户登录后跳转到该url
+                query: {redirect: to.fullPath}
+            })
+        }
+    } else {
+        // 不需要登录权限，直接进入
+        next()
+    }
+})
+export default router
